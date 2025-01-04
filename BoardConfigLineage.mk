c0239facffeec42f31234d3f2130c0ad6127082a
nhansp <nhan.094.vn@gmail.com>
2025-01-05 06:08:29 +0700

    BUILD_BROKEN_DUP_RULES -> true
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+
 # Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_kernel_boot))
